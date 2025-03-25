e0c6295a6db57047efb478b88f6130b152cd64a5
huijuhnkim <huijuhnkim.dev@gmail.com>
2025-03-24 22:05:03 -0400

    Initial Commit

---
+++ BrainDumpMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrainDumpMobile
+//
+//  Created by Hui Juhn Kim on 3/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
